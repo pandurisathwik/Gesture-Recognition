17c4b5ad39f63a1687d5ea4875845be3b7b2fade
pandurisathwik <74193944+pandurisathwik@users.noreply.github.com>
2021-08-30 19:14:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function setup(){
+ video = createCapture(VIDEO);
+ video.size(550,500);
+
+ canvas = createCanvas(550,550);
+ canvas.position(560,150);
+
+ poseNet = ml5.poseNet(video , modelLoaded);
+ poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is Initiiallized!")
+}
+
+function draw(){
+    background("#808080");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }
+}
